d55b78592904584aa284d9427593a3eb2a2af056
medxkh <medkh6699@gmail.com>
2020-12-21 13:58:07 +0100

    moove to selenium

--- price_tracker.py
+++ price_tracker.py
@@ -1,10 +1,15 @@
-import requests
+from selenium import webdriver
 
 URL="https://www.amazon.fr/Megaport-3-70-5-30GHz-GeForce-Centrale-Ordinateur/dp/B08FTHCRR5/"
 title_id="productTitle"
 price_id="priceblock_saleprice"
-image_url_id="https://images-na.ssl-images-amazon.com/images/I/61u8hpl1AHL._AC_SX679_.jpg"
+image_url_id="imgTagWrapperId"
 
-r = requests.get(URL)
-html_str=r.text
-print(html_str)
+CHROMEDRIVER= "/User/Applications/chromedriver"
+driver = webdriver.Chrome(CHROMEDRIVER)
+
+driver.get(URL)
+
+product_title=driver.find_element_by_id(tilte_id).text
+product_price=driver.find_element_by_id(price_id).text
+product_image_url=driver.find_element_by_id(image_url_id).get_attribute('src')
